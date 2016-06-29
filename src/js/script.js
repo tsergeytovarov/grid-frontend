5c35f68c4511ac0e547c440ebdbbe3c9c85c911b
Sergey Popov <tovarov.piter@gmail.com>
2016-06-29 19:56:29 +0300

    Javascript

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,32 @@
 
 (function(){
 
-  console.log("Я жив!");
+  var postContainer = document.querySelector('.post');
+  var temeSelect = document.querySelector('.js-change-teme');
+
+  if (temeSelect) {
+    temeSelect.addEventListener('change', function(evt) {
+      evt.preventDefault();
+      postContainer.classList = '';
+      postContainer.classList.add('post');
+      postContainer.classList.add(temeSelect.value);
+    });
+  }
+
+  var containerControl = document.querySelectorAll('.js-container');
+
+  if (containerControl) {
+    containerControl = Array.prototype.slice.call(containerControl, 0);
+    var container = document.querySelector('.container');
+
+    containerControl.forEach(function(item) {
+      item.addEventListener('click', function(evt) {
+        evt.preventDefault();
+        container.classList = '';
+        container.classList.add('container');
+        container.classList.add('container--' + item.dataset.value);
+      });
+    });
+  }
 
 })();
